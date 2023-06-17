cca3be58dc4c0f0a6232f92209bf193d1b0e4820
Daiky <enikyasta@debian-BULLSEYE-live-builder-AMD64>
2023-06-17 11:50:39 -0600

    ava

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './auth/pages/log-in/log-in.component';
 
 const routes: Routes = [
-  
   {
     path: 'auth',
     loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)
